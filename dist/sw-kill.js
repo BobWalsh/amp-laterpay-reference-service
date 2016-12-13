11d6a34502d1ad8a9bff96b90cc8348d96f07890
Tiago Rodrigues <trodrigues@laterpay.net>
2016-12-13 17:42:50 +0100

    remove dist amp

--- dist/sw-kill.js
+++
@@ -1,3 +0,0 @@
-(function(){self.addEventListener("install",function(a){a.waitUntil(self.skipWaiting())});self.addEventListener("activate",function(a){var b=clients.claim().then(function(){return caches.delete("cdn-js")}).then(function(){return new Promise(function(a,b){var c=indexedDB.deleteDatabase("cdn-js");c.onsuccess=a;c.onerror=b})});a.waitUntil(b)});})();
-//# sourceMappingURL=sw-kill.js.map
-
--- dist/v0/amp-viewer-integration-latest.js
+++
@@ -1,4 +0,0 @@
-(self.AMP = self.AMP || []).push({n:"amp-viewer-integration", f:(function(AMP) {
-})});
-//# sourceMappingURL=amp-viewer-integration-0.1.js.map
-
