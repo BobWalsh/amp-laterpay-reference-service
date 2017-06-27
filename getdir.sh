ab797165309fb51c776e5c68a3c52c8eac635009
Tiago Rodrigues <trodrigues@laterpay.net>
2017-06-27 18:02:25 +0200

    update getdir

--- getdir.sh
+++ getdir.sh
@@ -1,10 +1,32 @@
 #!/bin/bash
 
-name=$1
+addsandbox(name) {
+  cd $name
+  for i in $(seq 1 5) ; do
+  sed -i .bak 's/        "localeMessages"/        "sandbox": true,\
+          "localeMessages"/g' article$i.html
+  done
+  cd ..
+}
 
-cd $name
-curl -O https://ampexample.laterpay.net/$name/index.html
-for i in $(seq 1 5) ; do
-  curl -O https://ampexample.laterpay.net/$name/article$i.html
-done
-cd ..
+addsandbox('.')
+addsandbox('business')
+addsandbox('sports')
+addsandbox('culture')
+addsandbox('politics')
+
+exit 0
+
+getdir(name) {
+  cd $name
+  curl -O https://ampexample.laterpay.net/$name/index.html
+  for i in $(seq 1 5) ; do
+    curl -O https://ampexample.laterpay.net/$name/article$i.html
+  done
+  cd ..
+}
+
+getdir('business')
+getdir('sports')
+getdir('culture')
+getdir('politics')
